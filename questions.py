696b5b33d233e4f2c2ab87b982af4eaa118d941f
Delfina Diacinti <ddiacinti@gmail.com>
2026-03-22 20:09:24 -0300

    2da funcionalidad: agrego puntaje

--- questions.py
+++ questions.py
@@ -16,6 +16,8 @@ attempts = 6
 print("¡Bienvenido al Ahorcado!")
 print()
 
+puntaje = 0
+
 while attempts > 0:
     # Mostrar progreso: letras adivinadas y guiones para las que faltan
     progress = ""
@@ -29,6 +31,7 @@ while attempts > 0:
     # Verificar si el jugador ya adivinó la palabra completa
     if "_" not in progress:
         print("¡Ganaste!")
+        puntaje = puntaje + 6
         break
 
     print(f"Intentos restantes: {attempts}")
@@ -45,9 +48,13 @@ while attempts > 0:
     else:
         guessed.append(letter)
         attempts -= 1
+        puntaje = puntaje - 1
         print("Esa letra no está en la palabra.")
 
     print()
 
 else:
     print(f"¡Perdiste! La palabra era: {word}")
+    puntaje = 0
+
+print (f"Tu puntaje fue de: {puntaje}")
